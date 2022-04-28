0a11b37f674c0e4f43fd2658b63a5ea16e2ef08a
Kornelija Gasparaityte <kornelija.gasparaityte@gmail.com>
2022-04-28 20:11:31 +0300

    Finished with React core features

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -7,7 +7,7 @@ import classes from "./ErrorModal.module.css";
 const ErrorModal = (props) => {
   return (
     <div>
-      <div className={classes.backdrop} />
+      <div className={classes.backdrop} onClick={props.onConfirm}/>
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -16,7 +16,7 @@ const ErrorModal = (props) => {
           <p>{props.message}</p>
         </div>
         <footer className={classes.actions}>
-          <Button>Okay</Button>
+          <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
     </div>
@@ -74,5 +74,57 @@ it's actually being triggered, which is in AddUser.js
 P.S. Tutorial author said it's ok to render it elsewhere
 but we have to keep in mind that we will then have to
 update our state management accordingly.
+
+
+3) Error State management
+
+We need a separate State for error display
+for our program to know when to show the ErrorModal.
+
+Where?
+
+The State should be in the same component that we render
+the ErrorModal, and as we've decided it will be in AddUser.js.
+
+What values?
+
+The error State can be a string - like the error message,
+or it could be an object which bundles the error title and error message,
+anything we want.
+
+But basically we need the Error State to have two possible states:
+ - "We have an error", in which case the ErrorModal is rendered;
+ - "We don't have an error", in which case the ErrorModal isn't rendered.
+
+The tutorial author is going to use objects for the state values,
+so I'm following that.
+
+
+4) Dismissing the ErrorModal
+
+Now, we also want to make it possible to dismiss the ErrorModal
+by either clicking on the "Okay" button or by clicking on the backdrop.
+
+For that we need to clear our error State in AddUser.js
+since the AddUser JSX logic checks whether the value of the error State
+is truthy, and if it is, then displays the ErrorModal:
+{error && <ErrorModal title={error.title} message={error.message} />}
+
+So the only way of getting rid of the ErrorModal is to re-set the
+error to undefined like it initialy is, or to null, or any other falsy value.
+
+So we create this function in AddUser.js:
+
+ const errorHandler = () => {
+    setError(null);
+  };
+
+And we pass a pointer to it to the ErrorModal through a prop:
+{error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
+
+Then here in ErrorModal.js 
+we add an onClick prop to both the custom <Button> and the <div> that acts as the backdrop.
+Reminder: the onClick prop is a built-in prop on every HTML-like element in JSX, 
+not just on <button>.
 
 */
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,13 +8,27 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
+  /*
+  There is no need to pass an initial value to the error State because
+  if it's empty, it means there is no error. 
+  The value is then 'undefined', which is a valid value type in JS.
+  */
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values).'
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0).'
+      });
       return;
     }
     //console.log(enteredUsername, enteredAge);
@@ -32,9 +46,13 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title="An error occurred!" message="Something went wrong!" />
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
